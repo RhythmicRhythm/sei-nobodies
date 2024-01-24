4f4cd42bb68fc1ae334ae38d2b128c1c1e67e802
RhythmicRhythm <Okubadejorh@gmail.com>
2024-01-24 19:56:00 +0100

    +

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import TwitterLogin from 'react-twitter-login';
+import { authentication } from './firebase-config';
+import { getAuth, signInWithPopup, TwitterAuthProvider } from "firebase/auth";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  const onSuccess = (response) => {
-    console.log('Twitter login successful!', response);
-    // Store the access token or user data, or redirect to dashboard
-  };
 
-  const onFailure = (error) => {
-    console.error('Twitter login failed:', error);
-  };
+  const SigninTwitter = () => {
+    const provider = new TwitterAuthProvider();
+    signInWithPopup(authentication, provider)
+    .then((re) => {
+      console.log(re);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+
+  }
 
   return (
     <>
    <div className="">
     hello
-      <TwitterLogin
-      loginUrl="https://api.twitter.com/oauth/authenticate"
-      onFailure={onFailure}
-      onSuccess={onSuccess}
-      clientId="cQqW1802wFZNg1xBnu745Hkzs"
-      clientSecret="g8B9MiLOlZb7rMFP3nEhcf01cZWxBCHWlN5RBYigmT1IUqnO2b"
-    >
-      <button>Login with Twitter</button>
-    </TwitterLogin>
+    <button onClick={SigninTwitter}>
+      connect da twitter
+    </button>
    </div>
     </>
   )
