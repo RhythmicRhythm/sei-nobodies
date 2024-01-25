9b544acc1b9de58a83894dbabfc488f3b4721ea6
RhythmicRhythm <Okubadejorh@gmail.com>
2024-01-25 03:12:37 +0100

    +

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const About = () => {
   return (
     <div>
-        <section className="flex items-center bg-stone-100 xl:h-screen font-poppins dark:bg-gray-800 ">
+      <Navbar />
+        <section className="flex items-center xl:h-screen">
         <div className="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
           <div className="px-4 mb-10 md:text-center md:mb-20">
-            <p className="mb-2 text-lg font-semibold text-blue-500 dark:text-gray-400">
+            <p className="mb-2 text-xl font-semibold text-rose-700 dark:text-gray-400">
               About Us
             </p>
-            <h2 className="pb-2 text-2xl font-bold text-gray-800 md:text-4xl dark:text-gray-300">
-              What we do
+            <h2 className="pb-2 text-6xl font-bold text-gray-800 md:text-4xl dark:text-gray-300">
+              Why Sei Nobodies
             </h2>
             <div className="flex w-32 mt-1 mb-6 overflow-hidden rounded md:mx-auto md:mb-14">
-              <div className="flex-1 h-2 bg-blue-200">
+              <div className="flex-1 h-2 bg-rose-400">
               </div>
-              <div className="flex-1 h-2 bg-blue-400">
+              <div className="flex-1 h-2 bg-rose-600">
               </div>
-              <div className="flex-1 h-2 bg-blue-300">
+              <div className="flex-1 h-2 bg-rose-400">
               </div>
             </div>
           </div>
@@ -26,8 +28,8 @@ const About = () => {
               <img src="https://i.postimg.cc/j5L5bX2d/pexels-andrea-piacquadio-3757946.jpg" alt="" className="relative z-40 object-cover w-full h-96" />
             </div>
             <div className="w-full px-4 mb-10 lg:w-1/2 lg:mb-0 ">
-              <h2 className="py-3 pl-2 mb-4 text-2xl font-bold text-gray-700 border-l-4 border-blue-500 dark:border-blue-400 dark:text-gray-300">
-                We are providing a better facility
+              <h2 className="py-3 pl-2 mb-4 text-2xl font-bold text-gray-700 border-l-4 border-rose-500 dark:text-gray-300">
+                We are providing a Sei Sei Sei
               </h2>
               <p className="mb-4 text-base leading-7 text-gray-500 dark:text-gray-400">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
@@ -36,7 +38,7 @@ const About = () => {
               </p>
               <ul className="mb-10">
                 <li className="flex items-center mb-4 text-base text-gray-600 dark:text-gray-400">
-                  <span className="mr-3 text-blue-500 dark:text-blue-400 ">
+                  <span className="mr-3 text-rose-500">
                     <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentColor" className="w-5 h-5 bi bi-patch-check-fill" viewBox="0 0 16 16">
                       <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
                     </svg>
@@ -44,25 +46,32 @@ const About = () => {
                   Lorem ipsum dolor sit amet, consectetur domino act
                 </li>
                 <li className="flex items-center mb-4 text-base text-gray-600 dark:text-gray-400">
-                  <span className="mr-3 text-blue-500 dark:text-blue-400">
+                  <span className="mr-3 text-rose-500">
                     <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentColor" className="w-5 h-5 bi bi-patch-check-fill" viewBox="0 0 16 16">
                       <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
                     </svg>
                   </span>
-                  eli orem ipsum dolor sit amet, consectetur advice
+                  Lorem ipsum dolor sit amet, consectetur domino act
                 </li>
                 <li className="flex items-center mb-4 text-base text-gray-600 dark:text-gray-400">
-                  <span className="mr-3 text-blue-500 dark:text-blue-400 ">
+                  <span className="mr-3 text-rose-500">
                     <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentColor" className="w-5 h-5 bi bi-patch-check-fill" viewBox="0 0 16 16">
                       <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
                     </svg>
                   </span>
-                  Iron man ipsum dolor sit amet, consectetur adipiscing
+                  Lorem ipsum dolor sit amet, consectetur domino act
                 </li>
+                <li className="flex items-center mb-4 text-base text-gray-600 dark:text-gray-400">
+                  <span className="mr-3 text-rose-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentColor" className="w-5 h-5 bi bi-patch-check-fill" viewBox="0 0 16 16">
+                      <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
+                    </svg>
+                  </span>
+                  Lorem ipsum dolor sit amet, consectetur domino act
+                </li>
+              
               </ul>
-              <a href="#" className="px-4 py-3 text-blue-700 transition-all transform border border-blue-500 hover:bg-blue-600 dark:border-blue-400 dark:hover:bg-blue-500 dark:hover:text-gray-100 dark:hover:border-blue-500 dark:text-blue-400 hover:text-gray-100">
-                Discover more
-              </a>
+              
             </div>
           </div>
         </div>
